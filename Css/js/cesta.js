c6480a9e69addc98e1655e42fee7d98041efa0bf
williansbriones <wi.briones@duocuc.cl>
2024-06-07 02:59:38 -0400

    login

--- Css/js/cesta.js
+++ Css/js/cesta.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
    
     let lista = JSON.parse(localStorage.getItem("carro"))
-
+    console.log(lista)
     lista.forEach(Producto => {
+        let precio = Producto.price.replace("$", "")
+        console.log(getNombreasync(Producto.id))
         $(".informacioncompra").append(`
-            <div class="productoscompra" id="productoscompra">${getNombre(Producto.id)}</div>
-            <div class="total" id="total">${Producto.price * Producto.count}</div>
+            <div class="productoscompra" id="productoscompra">${"a"}</div>
+            <div class="total" id="total">${precio* Producto.count}</div>
             `)
     });
 });
 
 
 function getNombre(id){
-
+    return new Promise((resolve, reject) => {
     $.ajax({
         url: 'http://localhost:8081/api/Producto/'+id, // Reemplaza con la URL de tu API
         type: 'GET',
-        success: function() {
-            console.log(response)
-            localStorage.setItem("user", JSON.stringify(response))
+        success: function(response) {
             resolve(response);
         },
         error: function(error) {
@@ -26,4 +26,15 @@ function getNombre(id){
             reject(error);
         }
     });
+}
+)}
+
+async function getNombreasync(id) {
+    try {
+        let elemento = await getNombre(id);
+        return elemento
+    } catch (error) {
+        console.error(error);
+        // Puedes manejar el error aquí, por ejemplo, continuar con el siguiente producto o detener la ejecución
+    }
 }
